9d7528ee6a6f60d4630812f531c67ebb9aa67252
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-09-23 01:28:48 -0300

    [MimicAPI] Palavra Controller API Verbos e Rotas

---
+++ Secao 01/01_fim/MimicAPI/MimicAPI/Controllers/PalavrasController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MimicAPI.Database;
+using MimicAPI.Models;
+
+namespace MimicAPI.Controllers
+{
+    [Route("/api/palavras")]
+    public class PalavrasController : ControllerBase
+    {
+        private readonly MimicContext _banco;
+
+        public PalavrasController(MimicContext banco)
+        {
+            _banco = banco;
+        }
+
+        // APP -- /api/palavras
+        [Route("")]
+        [HttpGet]
+        public ActionResult ObterTodas()
+        {
+            return Ok(_banco.Palavras);
+        }
+
+        // Web -- /api/palavras/1
+        [Route("{id}")]
+        [HttpGet]
+        public ActionResult Obter(int id)
+        {
+            return Ok(_banco.Palavras.Find(id));
+        }
+
+        // -- /api/palavras (POST: id, nome, ativo, pontuacao, criacao)
+        [Route("")]
+        [HttpPost]
+        public ActionResult Cadastrar([FromBody] Palavra palavra)
+        {
+            _banco.Palavras.Add(palavra);
+            _banco.SaveChanges();
+            return Ok();
+        }
+
+        // -- /api/palavras/1 (PUT: id, nome, ativo, pontuacao, criacao)
+        [Route("{id}")]
+        [HttpPut]
+        public ActionResult Atualizar(int id, [FromBody] Palavra palavra)
+        {
+            palavra.Id = id;
+            _banco.Palavras.Update(palavra);
+            _banco.SaveChanges();
+            return Ok();
+        }
+
+        // -- /api/palavras/1 (DELETE)
+        [Route("{id}")]
+        [HttpDelete]
+        public ActionResult Deletar(int id)
+        {
+            //_banco.Palavras.Remove(_banco.Palavras.Find(id));
+            var palavra = _banco.Palavras.Find(id);
+            palavra.Ativo = false;
+            _banco.Palavras.Update(palavra);
+            _banco.SaveChanges();
+            return Ok();
+        }
+    }
+}
