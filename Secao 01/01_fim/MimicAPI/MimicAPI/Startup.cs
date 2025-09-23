ac6fa494b6a28137b9446674c03ae346773adfae
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-09-22 23:52:58 -0300

    [MimicAPI] Configurar o MVC e Cria a Entidade Palavra, Banco de dados e MimicContext

--- Secao 01/01_fim/MimicAPI/MimicAPI/Startup.cs
+++ Secao 01/01_fim/MimicAPI/MimicAPI/Startup.cs
@@ -1,11 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MimicAPI.Database;
 
 namespace MimicAPI
 {
@@ -21,6 +19,9 @@ namespace MimicAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MimicContext>( opt => {
+                opt.UseSqlite("Data Source=Database\\Mimic.db");
+            });
             services.AddMvc();
         }
 
