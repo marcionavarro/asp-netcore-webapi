a2e9ddb1bc59f8b143e16374db7b86cab8499810
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-09-26 23:08:14 -0300

    [MimicAPI] Http Status Code, Filtro por data, Paginação

--- Secao 01/01_fim/MimicAPI/MimicAPI/Controllers/PalavrasController.cs
+++ Secao 01/01_fim/MimicAPI/MimicAPI/Controllers/PalavrasController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MimicAPI.Database;
+using MimicAPI.Helpers;
 using MimicAPI.Models;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
 
 namespace MimicAPI.Controllers
 {
@@ -17,9 +21,35 @@ namespace MimicAPI.Controllers
         // APP -- /api/palavras
         [Route("")]
         [HttpGet]
-        public ActionResult ObterTodas()
+        public ActionResult ObterTodas([FromQuery]PalavraUrlQuery query)
         {
-            return Ok(_banco.Palavras);
+            var item = _banco.Palavras.AsQueryable();
+
+            if (query.Data.HasValue)
+            {
+                item = item.Where(a => a.Criado > query.Data.Value || a.Atualizado > query.Data.Value);
+            }
+
+            if (query.PagNumero.HasValue)
+            {
+                var quantidadeTotalRegistros = item.Count();
+                item = item.Skip((query.PagNumero.Value - 1) * query.PagRegistro.Value).Take(query.PagRegistro.Value);
+
+                var paginacao = new Paginacao();
+                paginacao.NumeroPagina = query.PagNumero.Value;
+                paginacao.RegistroPorPagina = query.PagRegistro.Value;
+                paginacao.TotalRegistros = quantidadeTotalRegistros;
+                paginacao.TotalPaginas = (int)Math.Ceiling((double)quantidadeTotalRegistros / query.PagRegistro.Value);
+
+                Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(paginacao));
+
+                if (query.PagNumero > paginacao.TotalPaginas)
+                {
+                    return NotFound();
+                }
+            }
+
+            return Ok(item);
         }
 
         // Web -- /api/palavras/1
---
+++ Secao 01/01_fim/MimicAPI/MimicAPI/Helpers/Paginacao.cs
@@ -0,0 +1,10 @@
+﻿namespace MimicAPI.Helpers
+{
+    public class Paginacao
+    {
+        public int NumeroPagina { get; set; }
+        public int RegistroPorPagina { get; set; }
+        public int TotalRegistros { get; set; }
+        public int TotalPaginas { get; set; }
+    }
+}
