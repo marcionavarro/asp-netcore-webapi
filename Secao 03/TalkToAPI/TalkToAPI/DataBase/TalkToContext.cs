f43c5e8759612062be4cd6c651210524f8c7486f
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-03-25 00:17:06 -0300

    [TalkToAPI] Configurando o Banco de dados e Modelos

---
+++ Secao 03/TalkToAPI/TalkToAPI/DataBase/TalkToContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TalkToAPI.V1.Models;
+
+namespace TalkToAPI.DataBase
+{
+    public class TalkToContext : IdentityDbContext<AplicationUser>
+    {
+        public TalkToContext(DbContextOptions<TalkToContext> options) : base(options)
+        { 
+        
+        }
+
+        public DbSet<Mensagem> Mensagem { get; set; }
+    }
+}
--- Secao 03/TalkToAPI/TalkToAPI/Startup.cs
+++ Secao 03/TalkToAPI/TalkToAPI/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -21,6 +22,10 @@ namespace TalkToAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataBase.TalkToContext>(cfg =>
+            {
+                cfg.UseSqlite("Data Source=Database\\TalkTo.db");
+            });
             services.AddMvc();
         }
 
